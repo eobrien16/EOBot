38cc727b29865575285c2350d453ca3b7f2127c9
Unknown <eobrien16@ardscoil.ie>
2018-04-24 20:04:21 +0100

    Updated EOBot

--- eobot.js
+++ eobot.js
@@ -41,33 +41,33 @@ client.on("message", async message => {
       let user = message.mentions.users.first();
       await member.kick(reason)
         .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-      await message.channel.send({embed: {
-      color: 1638655,
-      title: "Member Kicked",
-      thumbnail: {
-        url: user.avatarURL
-      },
-      fields: [{
-       name: "Member",
-       value: arguments[1]
-     },
-     {
-       name: "Moderator",
-       value: `${message.author}`
-     },
-     {
-       name: "Reason",
-       value: arguments[2]
-     }
-   ],
-   timestamp: new Date(),
-   footer: {
-     icon_url: client.user.avatarURL,
-     text: "EOBot ModUtils"
-   }
-    }})
-    return console.log(user.avatarURL);
-
+      for (i = 0; i <= 1; i++){
+        await message.channel.send({embed: {
+          color: 1638655,
+          title: "Member Kicked",
+          thumbnail: {
+            url: user.avatarURL
+          },
+          fields: [{
+              name: "Member",
+              value: arguments[1]
+            },
+            {
+              name: "Moderator",
+              value: `${message.author}`
+            },
+            {
+              name: "Reason",
+              value: arguments[2]
+            }
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: client.user.avatarURL,
+            text: "EOBot ModUtils"
+          }
+        }})
+      }
   } else if (message.content.startsWith(">avatar")) {
     var messagez = ' '
     await message.delete(0);
@@ -77,11 +77,11 @@ client.on("message", async message => {
       return message.reply(avtr);
     } else {
         let avtr = message.author.avatarURL;
-        return message.reply(avtr);
+        return message.channel.reply(avtr);
     }
   } else if (message.content.startsWith(">debug")){
     let user = message.mentions.users.first();
-    return message.channel.send({embed: {
+    return message.author.send({embed: {
     color: 1638655,
     title: "Henlo",
     description: arguments[1],
@@ -102,7 +102,7 @@ client.on("message", async message => {
     let rplc = cntnt.replace(/ /g, "+");
     return message.reply("http://lmgtfy.com/?q=" + rplc);
   } else if(message.content.startsWith(">purge")) {
-      const deleteCount = parseInt(arguments[1], 1);
+      const deleteCount = parseInt(arguments[1], 10);
       if(!deleteCount || deleteCount < 2 || deleteCount > 100){
         return message.channel.send({embed: {
           color: 16711680,
@@ -114,11 +114,62 @@ client.on("message", async message => {
           .catch(error => message.reply(`You completely fucked up here: ${error}`));
         return message.channel.send({embed: {
           color: 1638655,
-          description: "***mk, deleted " + deleteCount + " messages***"
+          description: "***mk, deleted " + deleteCount*10 + " messages***"
         }})
     }
   } else if (message.content.startsWith(">help")){
       await message.delete(0);
+      if (arguments[1] == "general"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>general```***"
+        }})
+      } else if (arguments[1] == "ai-general"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>ai-general```***"
+        }})
+      } else if (arguments[1] == "kick"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>kick [user] [reason]```***"
+        }})
+      } else if (arguments[1] == "avatar"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>avatar [user]```***"
+        }})
+      } else if (arguments[1] == "debug"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>debug```***"
+        }})
+      } else if (arguments[1] == "lmgtfy"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>lmgtfy [your query]```***"
+        }})
+      } else if (arguments[1] == "purge"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>purge [number between 2 and 100]```***"
+        }})
+      } else if (arguments[1] == "help"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>help [leave blank for list, or put in command]```***"
+        }})
+      } else if (arguments[1] == "ban"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>ban [username] [reason]```***"
+        }})
+      } else if (arguments[1] == "nou"){
+        return message.channel.send({embed: {
+          color: 1638655,
+          description: "***Usage: ```>nou [username]```***"
+        }})
+      } else {
       return message.channel.send({embed: {
         color: 1638655,
         title: "EOBot -- created with hate by eobsite1",
@@ -127,35 +178,43 @@ client.on("message", async message => {
         },
         fields: [{
          name: ">general",
-         value: "posts a message about general. ```Usage: >general```"
+         value: "posts a message about general."
        },
        {
          name: ">ai-general",
-         value: "posts a message about apple internal general. ```Usage: >ai-general```"
+         value: "posts a message about apple internal general."
        },
        {
          name: ">kick",
-         value: "fucks a member out of your server. ```Usage: >kick [user] [reason]```"
+         value: "fucks a member out of your server."
        },
        {
          name: ">avatar",
-         value: "lets you get your avatar or steal someone elses. ```Usage: >avatar [user]```"
+         value: "lets you get your avatar or steal someone elses."
        },
        {
          name: ">debug",
-         value: "See what the newest feature im testing is. ```Usage: >debug```"
+         value: "See what the newest feature im testing is."
        },
        {
          name: ">lmgtfy",
-         value: "If your dealing with a fucktard who doesnt know how to google. ```Usage: >lmgtfy [your query]```"
+         value: "If your dealing with a fucktard who doesnt know how to google."
        },
        {
          name: ">purge",
-         value: "purges your current channel. ```Usage: >purge [number between 2 and 100]```"
+         value: "purges your current channel."
        },
        {
          name: ">help",
-         value: "displays this message. ```Usage: >help```"
+         value: "displays this message."
+       },
+       {
+         name: ">ban",
+         value: "bans a person."
+       },
+       {
+         name: ">nou",
+         value: "lets you flex on someone with your no u."
        }
      ],
      timestamp: new Date(),
@@ -164,7 +223,74 @@ client.on("message", async message => {
        text: "EOBot ModUtils"
      }
     }})
-  } else if (message.content.startsWith(">")) {
+  }
+} else if (message.content.startsWith(">ban")) {
+  await message.delete(0);
+  if(!message.member.roles.some(r=>["Founder", "Administrator", "Moderator"].includes(r.name)) )
+    return message.channel.send({embed: {
+      color: 16711680,
+      description: "***Your not a high enough rank you inbred piece of shit.***"
+    }})
+
+  let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+  if(!member)
+    return message.channel.send({embed: {
+      color: 16711680,
+      description: "***Pick a member you fucking vegetable.***"
+    }})
+  if(!member.bannable)
+    return message.channel.send({embed: {
+      color: 16711680,
+      description: "***Don't try to ban higher ranks fucknut.***"
+    }})
+  let reason = args.slice(1).join(' ');
+  if(!reason) reason = "No reason provided";
+    let user = message.mentions.users.first();
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    for (i = 0; i <= 1; i++){
+      return message.channel.send({embed: {
+        color: 1638655,
+        title: "Member Banned",
+        image: {
+          url: user.avatarURL
+        },
+        fields: [{
+            name: "Member",
+            value: arguments[1]
+          },
+          {
+            name: "Moderator",
+            value: `${message.author}`
+          },
+          {
+            name: "Reason",
+            value: arguments[2]
+          }
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: client.user.avatarURL,
+          text: "EOBot ModUtils"
+        }
+      }})
+    }
+
+} else if (message.content.startsWith(">nou")) {
+  await message.delete(0);
+  let user = message.mentions.members.first()
+  if(!user)
+    return message.channel.send({embed: {
+      color: 16711680,
+      description: "***Pick a fucking member you gangreous whore.***"
+    }})
+  await message.author.send("you flexed on " + arguments[1])
+  await user.send("You were flexed on by " + message.author)
+  return message.channel.send({embed: {
+    color: 1638655,
+    description: arguments[1] + " no u."
+  }})
+} else if (message.content.startsWith(">")) {
       await message.delete(0);
       return message.reply("***Thats not a command you ignorant fuck.***");
   }
